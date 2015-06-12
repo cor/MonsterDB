d1479011019213d6a64a6f4e4adea06bedcc0d98
CorPruijs <cor@pruys.com>
2015-06-12 18:22:15 +0200

    index.html is now index.php

---
+++ website/index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+        <head>
+                <title>Monsters Inc. Company Website</title>
+                <link rel="stylesheet" href="css/styles.css">
+        </head>
+        <body>
+                <h1>Welcome Monsters</h1>
+                Monsters Inc. &copy; <?php echo date(Y) ?>
+                <script src="js/scripts.js"></script>
+        </body>
+</html>
