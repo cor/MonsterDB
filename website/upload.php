16207475015adece8fcde8a4e7a21741b27d29af
Jurriaan Pruijs <jurriaan@pruijs.nl>
2015-06-15 20:48:24 +0200

    Add upload

---
+++ website/upload.php
@@ -0,0 +1,43 @@
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $uploadOk = 1;
+}
+// Check if file already exists
+// if (file_exists($target_file)) {
+//     echo "Sorry, file already exists.";
+//     $uploadOk = 0;
+// }
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "xls" && $imageFileType != "xlsx" ) {
+    echo "Sorry '" . $imageFileType . "'' is not allowed, only XLS & XLSB files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+
+        echo "Deleting herkansing.."
+        // $result = mysqli_query($conn, "DELETE FROM herkansing");
+        // if(!$result){
+        //     die ('Can\'t delete herkansing : ' . mysql_error());
+        // }
+
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+?>
