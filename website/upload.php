55e99285e12e0d1b1b83eefb3c7fb3d7797d0306
Jurriaan Pruijs <jurriaan@pruijs.nl>
2015-06-15 21:17:56 +0200

    Add parsing of xls

--- website/upload.php
+++ website/upload.php
@@ -1,4 +1,21 @@
 <?php
+require('php-excel-reader/excel_reader2.php');
+
+require('SpreadsheetReader.php');
+
+$servername = "localhost";
+$username = "root";
+$password = "M0nsters";
+$database = "herkansingen";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
@@ -30,11 +47,35 @@ if ($uploadOk == 0) {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
 
-        echo "Deleting herkansing.."
-        // $result = mysqli_query($conn, "DELETE FROM herkansing");
-        // if(!$result){
-        //     die ('Can\'t delete herkansing : ' . mysql_error());
-        // }
+        // delete
+        $result = mysqli_query($conn, "DELETE FROM herkansing");
+        if(!$result){
+            die ('Can\'t delete herkansing : ' . mysql_error());
+        }
+
+        // load
+
+        $Reader = new SpreadsheetReader($target_file);
+        $Sheets = $Reader -> Sheets();
+
+        foreach ($Sheets as $Index => $Name)
+        {
+            if ($Name == "Blad1") {
+                $Reader -> ChangeSheet($Index);
+
+                foreach ($Reader as $Row)
+                {
+                    $nummer = $Row[0];
+                    $achternaam = $Row[1];
+                    $voornaam = $Row[2];
+                    $studie = $Row[3];
+                    $vak = $Row[4];
+
+                    echo "<br/>nummer: ".$nummer." achternaam: ".$achternaam." voornaam: ".$voornaam." studie: ".$studie." vak: ".$vak;
+
+                }
+            }
+        }
 
     } else {
         echo "Sorry, there was an error uploading your file.";
